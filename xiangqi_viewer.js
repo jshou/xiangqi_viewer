c8a87c4db17707db8753b276ee276993b2f4be74
Sebastian Pipping <sebastian@pipping.org>
2022-03-21 23:03:11 +0100

    Use relative (rather than absolute) path to SVG images (fixes #13)

--- xiangqi_viewer.js
+++ xiangqi_viewer.js
@@ -512,9 +512,9 @@ XiangqiViewer.Chariot = function(red) {
   me.red = red;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/chariot_red.svg";
+      return "images/xiangqi_viewer/chariot_red.svg";
     } else {
-      return "/images/xiangqi_viewer/chariot_black.svg";
+      return "images/xiangqi_viewer/chariot_black.svg";
     }
   };
 
@@ -527,9 +527,9 @@ XiangqiViewer.Horse = function(red) {
   me.red = red;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/horse_red.svg";
+      return "images/xiangqi_viewer/horse_red.svg";
     } else {
-      return "/images/xiangqi_viewer/horse_black.svg";
+      return "images/xiangqi_viewer/horse_black.svg";
     }
   };
 
@@ -562,9 +562,9 @@ XiangqiViewer.Elephant = function(red) {
   me.distance = 2;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/elephant_red.svg";
+      return "images/xiangqi_viewer/elephant_red.svg";
     } else {
-      return "/images/xiangqi_viewer/elephant_black.svg";
+      return "images/xiangqi_viewer/elephant_black.svg";
     }
   };
 
@@ -578,9 +578,9 @@ XiangqiViewer.Advisor = function(red) {
   me.distance = 1;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/adviser_red.svg";
+      return "images/xiangqi_viewer/adviser_red.svg";
     } else {
-      return "/images/xiangqi_viewer/adviser_black.svg";
+      return "images/xiangqi_viewer/adviser_black.svg";
     }
   };
 
@@ -593,9 +593,9 @@ XiangqiViewer.General = function(red) {
   me.red = red;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/general_red.svg";
+      return "images/xiangqi_viewer/general_red.svg";
     } else {
-      return "/images/xiangqi_viewer/general_black.svg";
+      return "images/xiangqi_viewer/general_black.svg";
     }
   };
 
@@ -608,9 +608,9 @@ XiangqiViewer.Pawn = function(red) {
   me.red = red;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/pawn_red.svg";
+      return "images/xiangqi_viewer/pawn_red.svg";
     } else {
-      return "/images/xiangqi_viewer/pawn_black.svg";
+      return "images/xiangqi_viewer/pawn_black.svg";
     }
   };
 
@@ -623,9 +623,9 @@ XiangqiViewer.Cannon = function(red) {
   me.red = red;
   me.spriteUrl = function() {
     if (red) {
-      return "/images/xiangqi_viewer/cannon_red.svg";
+      return "images/xiangqi_viewer/cannon_red.svg";
     } else {
-      return "/images/xiangqi_viewer/cannon_black.svg";
+      return "images/xiangqi_viewer/cannon_black.svg";
     }
   };
 
